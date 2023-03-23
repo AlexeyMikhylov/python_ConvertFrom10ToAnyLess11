ad85c5066c3df7156f1c641cd2d9d0df3e319e9e
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2023-03-23 13:26:19 +0300

    up
    python file

---
+++ ConvertationFrom10ToAnyIn2-10.py
@@ -0,0 +1,24 @@
+# except any more than 10
+# basically to any scale in 2-10
+
+def From10ToAny(a, ScaleOfNotation):
+    b = ''
+    while a > 0:
+        for i in range (0, len(str(a))):
+            b += str(a % ScaleOfNotation)
+            a //= ScaleOfNotation
+    return b[::-1]
+
+#tests
+print( From10ToAny(112, 8) )
+print( From10ToAny(1000, 8) )
+print( From10ToAny(1000, 9) )
+print( From10ToAny(654, 3) )
+print( From10ToAny(100, 2) )
+print( From10ToAny(48, 10) )
+print( From10ToAny(64, 2) )
+print( From10ToAny(10, 2) )
+print( From10ToAny(6, 6) )
+print( From10ToAny(6, 6) )
+print( From10ToAny(78, 7) )
+print( From10ToAny(10, 16) ) #not working, primitive function
